061ac3e62d6903e4f203fcad7b26077254c180a9
Warren Falk <warren@warrenfalk.com>
2020-03-11 21:36:49 -0400

    v1.0.2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+"use strict";
+var path = require("path");
 function findRoot() {
     var r = module;
-    while (r.parent) { r = r.parent };
+    while (r.parent) {
+        r = r.parent;
+    }
     return r.path || path.dirname(r.filename);
 }
-
 module.exports = {
-    path: findRoot(),
-}
+    path: findRoot()
+};
